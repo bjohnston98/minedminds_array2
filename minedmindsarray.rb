0f4ddf78272b7239a1d07d103c8c0a5d23bb4b23
Brian Johnston <b_johnston98@yahoo.com>
2017-01-21 16:27:43 -0500

    good final code, no puts

--- minedmindsarray.rb
+++ minedmindsarray.rb
@@ -1,10 +1,27 @@
+##Good One
 def create_mined_minds_array()
-	 #create empty array, but defined it as follows
-	mined_minds_array = [] #now populate 100x
-	100.times do
-		#push itemsinto array
-		mined_minds_array << 'array item'
+	
+	mined_minds_array = []
+
+	num = 1 
+	100.times do 
+		if num % 15 == 0
+			mined_minds_array << 'mined minds'
+
+		elsif num % 5 == 0
+		mined_minds_array << 'minds'
+
+		elsif num % 3 == 0
+		mined_minds_array << 'mined'
+
+		else  
+				mined_minds_array << num
+		end
+
+		num += 1 
+		
 	end
-#the previous fills the array witih 100 items, then 
-	mined_minds_array
+	mined_minds_array 
 end
+
+	
--- testminedmindsarray.rb
+++ testminedmindsarray.rb
@@ -1,19 +1,42 @@
+##Good One
 require 'minitest/autorun' 
 require_relative 'minedmindsarray.rb' 
 
 class TestMMFunctions < Minitest::Test 
-
 	def test_one_equals_one()
 		assert_equal(1,1) 
 	end
 
-	def test_function_returns_array()
+	def test_2_function_returns_array() #sets value of array
 		results = create_mined_minds_array()
 		assert_equal(Array, results.class)
 	end
-	def test_function_returns_100()
+
+	def test_3_function_returns_100()
 		results = create_mined_minds_array()
 		assert_equal(100, results.length)
 	end
-end	#this seems to work
+	def test_3_function_returns_mined
+		results = create_mined_minds_array()
+		assert_equal('mined',results[2]) #array, first pos is 0 so increment
+	end
 
+	def	test_5_function_returns_minds
+		results = create_mined_minds_array()
+		assert_equal('minds', results[4])
+	end
+
+	def test_15_function_returns_minedminds
+		results = create_mined_minds_array()
+		assert_equal('mined minds', results[14])
+	end
+	
+end	
+
+#if num == 3
+#		'mined'
+#	elsif num % 3 == 0 #any num divis by 3, remain 0, returns 'mined'
+#	'mined'
+#	else
+#	num = num + 1
+#end
